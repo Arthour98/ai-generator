876def397f49bc64d6a86b2a333c55b79a413b14
artourakos <artourbouloudis@gmail.com>
2025-10-12 23:20:56 +0300

    matrix fully done

--- frontend/ai-front/src/components/custom-components/matrix.js
+++ frontend/ai-front/src/components/custom-components/matrix.js
@@ -5,8 +5,9 @@ import { Box } from "@chakra-ui/react";
 
 export default function Matrix({ elem, start, stop, }) {
     const [multiplier, setMultiplier] = useState(1)
-    let [rows, setRows] = useState((rows) => Array(1).fill(0));
+    const [heightLimit, setHeightLimit] = useState(null);
     let [cols, setCols] = useState(Array(1).fill(0));
+    let [rows, setRows] = useState((rows) => Array(1).fill(null));
     let rowsRef = useRef([]);
     let colsRef = useRef([]);
 
@@ -21,6 +22,16 @@ export default function Matrix({ elem, start, stop, }) {
         return rowWidth;
     }
 
+    const getElemsHeight = (elem) => {
+        let elemHeight = elem?.current.offsetHeight;
+        return elemHeight;
+    }
+
+    const getRowHeight = (row) => {
+        let rowHeight = row?.offsetHeight;
+        return rowHeight;
+    }
+
 
 
     useEffect(() => {
@@ -28,29 +39,34 @@ export default function Matrix({ elem, start, stop, }) {
         if (!row || !elem?.current) return;
 
         let multiplier = Math.round(getElemsWidth(elem) / getRowWidth(row))
+        let heightLimit = (getElemsHeight(elem) / getRowHeight(row)) + 1;
 
         if (multiplier !== 0) setMultiplier(multiplier);
-    }, [rowsRef])
+        if (heightLimit !== 0) setHeightLimit(heightLimit);
+    }, [rowsRef]);
 
     useLayoutEffect(() => {
         setRows(() => Array(multiplier).fill(0));
     }, [multiplier])
 
     useEffect(() => {
-
         const timer = setTimeout(() => {
-            const newCol = Math.floor(Math.random() * 2);
-            let newCols = [newCol, ...cols]
-
-            setCols(newCols)
-
-
+            let newNumber = Math.floor(Math.random() * 2);
+            let newCol = [newNumber, ...cols]
+            if (newCol.length > heightLimit) {
+                newCol.pop();
+            }
+            setCols(newCol);
         }, 500)
         console.log(cols);
-
+        console.log(heightLimit);
+        return () => clearTimeout(timer);
     }, [cols])
 
 
+    const shuffleNumbers = (col) => {
+        return Math.floor(Math.random() * 2);
+    }
 
     return (
         <div className={styles.matrixContainer}>
@@ -59,10 +75,13 @@ export default function Matrix({ elem, start, stop, }) {
                     return (
                         <div key={index} ref={(r) => rowsRef.current[index] = r} className={styles.rows}>
                             {
-                                cols?.map((col, index) => {
+                                cols?.map((col, indexC) => {
+
                                     return (
-                                        <div key={index} ref={(r) => colsRef.current[index] = r} className={styles.verticalCol}>
-                                            <p>{col}</p>
+                                        <div key={indexC} ref={(r) => colsRef.current[index] = r} className={styles.verticalCol}>
+                                            <p className={Math.floor(Math.random() * 2) === 1 ? `${styles.flicker}` : ""}
+                                            >{shuffleNumbers(col)}
+                                            </p>
                                         </div>
                                     )
                                 })

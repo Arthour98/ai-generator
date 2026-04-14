61b97d5dc4474b61af423247a0e0bfc8515e25fa
artourakos <artourbouloudis@gmail.com>
2026-04-14 11:35:44 +0300

    cors config to prod

--- backend/laravel/config/cors.php
+++ backend/laravel/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000','https://ai-gen-orcin.vercel.app',],
 
     'allowed_origins_patterns' => [],
 

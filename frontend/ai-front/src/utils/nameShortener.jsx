77081b26eddf87cb7a6b1aa90c7f7b60f4869b1f
artourakos <artourbouloudis@gmail.com>
2026-04-15 18:13:02 +0300

    correcting the function nameshortener

--- frontend/ai-front/src/utils/nameShortener.jsx
+++ frontend/ai-front/src/utils/nameShortener.jsx
@@ -2,10 +2,10 @@
 export const nameShortener = (str, length = 12) => {
     let newStr;
     if (str?.length >= 12) {
-        let newStr = str?.slice(1, length - 1) + "...";
+        let newStr = str?.slice(0, length - 1) + "...";
         return newStr
     }
     else {
-        return str?.slice(1, length - 1);
+        return str?.slice(0, length - 1);
     }
 }
